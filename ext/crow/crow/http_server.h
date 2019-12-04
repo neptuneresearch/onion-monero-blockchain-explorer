efffabae1d714a8ae79dbe34c226df2e3c6d51ff
neptuneresearch <neptuneresearch@protonmail.com>
2019-12-04 04:38:54 +0000

    Warnings debug 4.2

--- ext/crow/crow/http_server.h
+++ ext/crow/crow/http_server.h
@@ -152,7 +152,7 @@ namespace crow
                           << " using " << concurrency_ << " threads";
             CROW_LOG_INFO << "Call `app.loglevel(crow::LogLevel::Warning)` to hide Info level logs.";
             CROW_LOG_INFO << "noncesense-debug-6";
-            std::cout << "this is a debug cout message" << endl;
+            std::cout << "this is a debug cout message" << std::endl;
 
             signals_.async_wait(
                 [&](const boost::system::error_code& /*error*/, int /*signal_number*/){
